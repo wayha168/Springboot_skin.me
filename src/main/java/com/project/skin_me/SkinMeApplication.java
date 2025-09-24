86871baefb0daea0d674b24a923b3f0f6508d785
wayha168 <vehaseng1122@example.com>
2025-09-24 23:14:56 +0700

    first commit

---
+++ src/main/java/com/project/skin_me/SkinMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.skin_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/skin_me/SkinMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skin_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
